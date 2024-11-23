51fd049dce27362569f2c1663d974d682f198b2a
Rowechen Zhong <rowechen@mit.edu>
2024-11-23 21:45:23 +0000

    some solutions. waffle

--- src/generate_nl_proofs/generate.py
+++ src/generate_nl_proofs/generate.py
@@ -6,68 +6,63 @@
 
 import json
 import os
+import time
 
 import openai
 
 client = openai.OpenAI(api_key=os.environ["OPENAI_API_KEY"])
 
-# LOAD_PATH = "/home/ubuntu/ohm-illinois/plasma-converter/datasets/minif2f.jsonl"
-# SAVE_PATH = "/home/ubuntu/ohm-illinois/plasma-converter/datasets/minif2f_nl_proofs.jsonl"
+LOAD_PATH = "/home/ubuntu/ohm-illinois/plasma-converter/datasets/minif2f.jsonl"
+SAVE_PATH = "/home/ubuntu/ohm-illinois/plasma-converter/datasets/minif2f_nl_proofs.jsonl"
 
-# Testing
-LOAD_PATH = "/home/ubuntu/ohm-illinois/plasma-converter/datasets/small_dataset.jsonl"
-SAVE_PATH = "/home/ubuntu/ohm-illinois/plasma-converter/datasets/small_dataset_nl_proofs.jsonl"
+# # Testing
+# LOAD_PATH = "/home/ubuntu/ohm-illinois/plasma-converter/datasets/small_dataset.jsonl"
+# SAVE_PATH = "/home/ubuntu/ohm-illinois/plasma-converter/datasets/small_dataset_nl_proofs.jsonl"
 
 # this is a JSONL file!!!
 problem_statements = [json.loads(line) for line in open(LOAD_PATH)]
 
 for problem_statement in problem_statements:
+    # if the file already exists, skip
+    if os.path.exists(f"/home/ubuntu/ohm-illinois/plasma-converter/datasets/nl_proofs/{problem_statement['name']}.json"):
+        # load the file
+        with open(f"/home/ubuntu/ohm-illinois/plasma-converter/datasets/nl_proofs/{problem_statement['name']}.json", "r") as f:
+            problem_statement = json.load(f)
+        continue
+
     successful_proof = False
     print(problem_statement["name"])
     print()
     print(problem_statement["informal_prefix"])
     print()
     print()
-    while not successful_proof:
-        informal_prefix = problem_statement["informal_prefix"]
-        # trim
-        informal_prefix = informal_prefix.strip()
-        assert informal_prefix.startswith(
-            "/--"), "The informal prefix must start with /--"
-        assert informal_prefix.endswith(
-            "-/"), "The informal prefix must end with -/"
-        informal_prefix = informal_prefix[3:-2]
+    informal_prefix = problem_statement["informal_prefix"]
+    # trim
+    informal_prefix = informal_prefix.strip()
+    assert informal_prefix.startswith(
+        "/--"), "The informal prefix must start with /--"
+    assert informal_prefix.endswith(
+        "-/"), "The informal prefix must end with -/"
+    informal_prefix = informal_prefix[3:-2]
 
-        response = client.chat.completions.create(
-            model="gpt-4o-mini",
-            messages=[{"role": "user", "content": informal_prefix}],
-        )
-        print(response.choices[0].message.content)
+    start_time = time.time()
+    response = client.chat.completions.create(
+        model="o1-preview",
+        messages=[{"role": "user", "content": informal_prefix}],
+    )
+    end_time = time.time()
+    print(f"Time taken: {end_time - start_time} seconds")
+    print(response.choices[0].message.content)
 
-        # print(response.choices[0].message.content)
+    problem_statement["natural_language_proof"] = response.choices[0].message.content
+    # time taken is a proxy of difficulty
+    problem_statement["time_taken"] = end_time - start_time
 
-        # problem_statement["natural_language_proof"] = response.choices[0].message.content
-        response_text = response.choices[0].message.content
-
-        # ask gpt-4o-mini to find any errors in the proof
-        response = client.chat.completions.create(
-            model="gpt-4o-mini",
-            messages=[{"role": "user", "content": informal_prefix},
-                      {"role": "assistant", "content": response_text},
-                      {"role": "user", "content": "Find any errors in the proof. If there are no errors, return 'No errors found'. If there are errors, return the errors and the location of the errors in the proof."}],
-        )
-
-        # print(response.choices[0].message.content)
-
-        if response.choices[0].message.content == "No errors found.":
-            successful_proof = True
-        else:
-            print(response.choices[0].message.content)
-            print("Errors found in proof. Retrying...")
-            continue
-
-        problem_statement["natural_language_proof"] = response_text
-        break
+    # save this to an individual file
+    os.makedirs(
+        "/home/ubuntu/ohm-illinois/plasma-converter/datasets/nl_proofs", exist_ok=True)
+    with open(f"/home/ubuntu/ohm-illinois/plasma-converter/datasets/nl_proofs/{problem_statement['name']}.json", "w") as f:
+        json.dump(problem_statement, f)
 
 json.dump(problem_statements, open(
     SAVE_PATH, "w"))
